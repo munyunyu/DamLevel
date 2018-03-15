68528ef719ab3ef06929d88106851e0b41593943
Virus <Virus@VIRUS-PC>
2018-03-15 12:48:38 +0200

    Add project files.

---
+++ DamLevelReport/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using DamLevelReport.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DamLevelReport.Controllers
+{
+   
+    public class HomeController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+         
+
+            var name = User.Identity.GetUserName();
+            var identity = new ClaimsIdentity();
+
+
+
+            ViewBag.rock = name;
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [Authorize(Roles = "User")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }     
+}
---
+++ DamLevelReport/Models/TestData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DamLevelReport.Models
+{
+    public class TestData
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Role { get; set; }
+
+       
+    }
+}
